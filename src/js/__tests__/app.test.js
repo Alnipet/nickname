81c359e11e03b409abdafc1d62f786e369c762f2
lex_petrov <a777petrov@gmail.com>
2022-07-16 23:14:14 +0300

    finished

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,44 @@
+/* eslint-disable comma-dangle */
 // eslint-disable-next-line object-curly-newline
 import Validator from '../app';
 
-test.each(['Bowerman', 'Swordsman', 'Lex191111', 'Ivan-Ivanov_89'])('Проверяем %p', (username) => {
+test.each(['Bowerman', 'swordsman', 'lex19-yes', 'Iva__no123vak'])(
+  'Проверяем %p, буквы, цифры, дефис и нижнее подчеркивание в середине строки разрешены',
+  (username) => {
+    const isValidate = new Validator(username);
+    expect(isValidate.validateUsername()).toBe(true);
+  }
+);
+
+test.each(['Bowerman1', 'sasha-', 'Masha_'])(
+  'Проверяем %p, цифры, дефис и нижнее подчеркивание в конце строки запрещены',
+  (username) => {
+    const isValidate = new Validator(username);
+
+    expect(() => isValidate.validateUsername()).toThrowError('No valide Username');
+  }
+);
+
+test.each(['777Vasya', '-John', '_Marra'])(
+  'Проверяем %p, цифры, дефис и нижнее подчеркивание в начале строки запрещены',
+  (username) => {
+    const isValidate = new Validator(username);
+
+    expect(() => isValidate.validateUsername()).toThrowError('No valide Username');
+  }
+);
+
+test.each(['boris1986ivanov'])('Проверяем %p, больше трех цифр подряд запрещено', (username) => {
   const isValidate = new Validator(username);
-  expect(isValidate.validateUsername()).toBe(true);
+
+  expect(() => isValidate.validateUsername()).toThrowError('No valide Username');
 });
+
+test.each(['po+s', 'lesha*petrov', 'geka/$@#reta'])(
+  'Проверяем %p, все кроме букв цифр дефиса и нижнего подчеркивания запрещено',
+  (username) => {
+    const isValidate = new Validator(username);
+
+    expect(() => isValidate.validateUsername()).toThrowError('No valide Username');
+  }
+);
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,13 @@ export default class Validator {
   }
 
   validateUsername() {
-    return / . /.exec(this.username) !== null;
+    const result = /^[A-Za-z]{1}([A-Za-z\-_]*(?<!\d)\d{0,3}[A-Za-z\-_]*)*[A-Za-z]{1}$|^[A-Za-z]$/g.test(this.username);
+
+    if (!result) {
+      // eslint-disable-next-line no-throw-literal
+      throw new Error('No valide Username');
+    }
+
+    return result;
   }
 }
